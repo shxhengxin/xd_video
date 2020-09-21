282a7ff93882eae4d87b24fd9edcce8afa9f3aea
shenhengxin <853043009@qq.com>
2020-09-21 20:00:44 +0800

    自定义Filter未登录json错误码提示

--- src/main/java/net/xdclass/demo_project/filter/LoginFilter.java
+++ src/main/java/net/xdclass/demo_project/filter/LoginFilter.java
@@ -1,7 +1,9 @@
 package net.xdclass.demo_project.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.xdclass.demo_project.domain.User;
 import net.xdclass.demo_project.service.impl.UserServiceImpl;
+import net.xdclass.demo_project.utils.JsonData;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
@@ -9,6 +11,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @ClassName : LoginFilter  //类名
@@ -18,6 +21,7 @@ import java.io.IOException;
  */
 @WebFilter(urlPatterns = {"/api/v1/pri/*"},filterName = "loginFilter")
 public class LoginFilter implements Filter {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         System.out.println("=======init LoginFilter=========");
@@ -33,15 +37,30 @@ public class LoginFilter implements Filter {
             token = req.getParameter("token");
         }
 
-        if(StringUtils.isEmpty(token)) {
-            return;
+        if(!StringUtils.isEmpty(token)) {
+            //判断token是否合法
+            User user = UserServiceImpl.sessionMap.get(token);
+            if(user != null) {
+                chain.doFilter(request,response);
+            }else {
+                renderJson(res,objectMapper.writeValueAsString(JsonData.buildError(-2,"登录失败,token无效")));
+            }
+        }else {
+            renderJson(res,objectMapper.writeValueAsString(JsonData.buildError(-3,"未登录")));
         }
-        //判断token是否合法
-        User user = UserServiceImpl.sessionMap.get(token);
-        if(user != null) {
-            chain.doFilter(request,response);
-        }
+
+
         
+    }
+
+    private void renderJson(HttpServletResponse response,String json) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        try(PrintWriter writer = response.getWriter()) {
+            writer.print(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
